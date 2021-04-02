75bf8ae792309f31b823577f328aebc6b49b26c5
Partho P. Das <parthopdas@live.com>
2021-04-03 05:53:37 +1000

    README + build

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Tipitaka variants
 
+BJT, CHS, PTS, RTE
+
+## Deployment
+
+http://d.pali.tools/t
+
+## Scripts
+
+- Run these from root of the repo
+
+- Build site
+
+  ```ps1
+  ./build.ps1
+  ```
---
+++ build.ps1
@@ -0,0 +1,28 @@
+$root = "t"
+mkdir "../$root" -Force | Out-Null
+
+$indexHtml = "../$root/index.html"
+
+"<!-- auto generated -->" | Out-File -FilePath $indexHtml -Encoding utf8BOM
+"<a href='/$root/wtp'>WTP</a><br />" | Out-File -FilePath $indexHtml -Encoding utf8BOM -Append
+
+Get-ChildItem -Directory 
+| ForEach-Object {    
+    $name = $_.Name
+    
+    Write-Host "Generating for $name"
+
+    "<a href='/$root/$name'>$name</a><br />" | Out-File -FilePath $indexHtml -Encoding utf8BOM -Append
+    $thisIndexHtml = "../$root/$name/index.html"
+    mkdir "../$root/$name" -Force | Out-Null
+
+    "<!-- auto generated -->" | Out-File -FilePath $thisIndexHtml -Encoding utf8BOM
+    "<ul>" | Out-File -FilePath $thisIndexHtml -Encoding utf8BOM -Append
+    Get-ChildItem $name
+    | ForEach-Object {
+        $fileName = $_.Name
+        "<li><a href='/$root/$name/$fileName'>$fileName</a></li>" | Out-File -FilePath $thisIndexHtml -Encoding utf8BOM -Append        
+        Copy-Item $_.FullName "../$root/$name"
+    }
+    "</ul>" | Out-File -FilePath $thisIndexHtml -Encoding utf8BOM -Append
+}
